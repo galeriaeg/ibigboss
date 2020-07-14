4d0fa501e2b25a5211628c36a2089560520afdae
Alexandre Henrique <galeriaeg@gmail.com>
2020-07-14 19:10:37 -0300

    JS atualizado
    JS atualizado

--- js/all.js
+++ js/all.js
@@ -1,3 +1,33 @@
+//VALOR INICIAL LOGIN
+function Esvazia(valor_campo){
+	if (valor_campo.value == valor_campo.defaultValue)
+	valor_campo.value='';
+}
+
+function padrão(valor_campo){
+	if (valor_campo.value == '')
+	valor_campo.value=valor_campo.defaultValue;
+}
+
+
+
+//EXIBE E OCULTA SENHA
+function pass(){
+	var p = document.getElementById("senha").type;
+	
+	if(p == "password"){
+		document.getElementById("senha").type="text";
+		document.getElementById("icoPass").src="img/icon-senha.png";
+	}
+	else{
+		document.getElementById("senha").type="password";
+		document.getElementById("icoPass").src="img/icon-senha-off.png";
+	}
+}
+
+
+
+
 function checar(v){
 	var valor = v;
 
@@ -28,4 +58,29 @@ function blockValor(n,v){
 		document.getElementById('boxValor').style="display:none";
 		}
 
+}
+
+
+
+
+function somente_numero(campo){  
+	var digits="0123456789-/."  
+	var campo_temp   
+    for (var i=0;i<campo.value.length;i++){  
+        campo_temp=campo.value.substring(i,i+1)   
+        if (digits.indexOf(campo_temp)==-1){  
+            campo.value = campo.value.substring(0,i);  
+		}  
+	}  
+}  
+
+
+function mascara(src, mask){
+	var i = src.value.length;
+	var saida = mask.substring(0,1);
+	var texto = mask.substring(i)
+	if (texto.substring(0,1) != saida)
+	{
+		src.value += texto.substring(0,1);
+	}
 }
